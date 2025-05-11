882bb3cee99569f2be589b694bf5c4e68f403f28
Logan Szajnecki <ljszajnecki@outlook.com>
2025-05-11 18:40:17 -0500

    cleanup

--- build/CMakeFiles/Fluidix.dir/DependInfo.cmake
+++ build/CMakeFiles/Fluidix.dir/DependInfo.cmake
@@ -8,14 +8,14 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/szajnecki/Projects/Fluidix/src/ConservedVariables.cpp" "CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/GasModel.cpp" "CMakeFiles/Fluidix.dir/src/GasModel.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/GasModel.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/GnuplotWriter.cpp" "CMakeFiles/Fluidix.dir/src/GnuplotWriter.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/GnuplotWriter.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/Grid1D.cpp" "CMakeFiles/Fluidix.dir/src/Grid1D.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/Grid1D.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/RusanovFlux.cpp" "CMakeFiles/Fluidix.dir/src/RusanovFlux.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/RusanovFlux.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/Solver1D.cpp" "CMakeFiles/Fluidix.dir/src/Solver1D.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/Solver1D.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/VTKWriter1D.cpp" "CMakeFiles/Fluidix.dir/src/VTKWriter1D.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/VTKWriter1D.cpp.o.d"
-  "/home/szajnecki/Projects/Fluidix/src/main.cpp" "CMakeFiles/Fluidix.dir/src/main.cpp.o" "gcc" "CMakeFiles/Fluidix.dir/src/main.cpp.o.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/ConservedVariables.cpp" "CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/GasModel.cpp" "CMakeFiles/Fluidix.dir/src/GasModel.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/GasModel.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/GnuplotWriter.cpp" "CMakeFiles/Fluidix.dir/src/GnuplotWriter.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/GnuplotWriter.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/Grid1D.cpp" "CMakeFiles/Fluidix.dir/src/Grid1D.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/Grid1D.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/RusanovFlux.cpp" "CMakeFiles/Fluidix.dir/src/RusanovFlux.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/RusanovFlux.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/Solver1D.cpp" "CMakeFiles/Fluidix.dir/src/Solver1D.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/Solver1D.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/VTKWriter1D.cpp" "CMakeFiles/Fluidix.dir/src/VTKWriter1D.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/VTKWriter1D.cpp.obj.d"
+  "C:/Users/logan/Desktop/Projects/Fluidix/src/main.cpp" "CMakeFiles/Fluidix.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Fluidix.dir/src/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Fluidix.dir/src/ConservedVariables.cpp.o: \
- /home/szajnecki/Projects/Fluidix/src/ConservedVariables.cpp \
- /usr/include/stdc-predef.h \
- /home/szajnecki/Projects/Fluidix/include/ConservedVariables.h
